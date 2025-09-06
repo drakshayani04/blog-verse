7e3e3bd9ec7c2de0400ce19186d60719abbf540c
drakshayani04 <kdrakshayani04@gmail.com>
2025-09-06 12:37:05 +0530

    commit

--- sample.js
+++ sample.js
@@ -1,1 +1,20 @@
 console.log("this is a file")
+let name = 'drakshayani';
+console.log(name)
+let n=18;
+console.log(n)
+console.log(typeof name)
+
+let islogged=false;
+console.log(islogged)
+console.log( typeof islogged)
+let x;
+console.log(typeof x)
+let y=null;
+console.log(y)
+let ar=[1,2,3,4]
+console.log(ar[0])
+console.log(typeof ar)
+let person={ name:'drakshayani',age:18}
+console.log(person.name)
+console.log(typeof person.name)

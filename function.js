8edb90fd43af42407dd0860c3487fbf03a132853
drakshayani04 <kdrakshayani04@gmail.com>
2025-09-06 15:54:48 +0530

    functiom

---
+++ function.js
@@ -0,0 +1,20 @@
+function greet()
+{
+    console.log("welcome to devastra")
+}
+function add(x,y)
+{
+    return x+y
+}
+result=add(5,4)
+console.log(result)
+const multiply=function(x,y)
+{
+    return x*y
+}
+console.log(multiply(5,3))
+const subtract=(x,y)=>{
+    console.log(x,y)
+    return x-y
+}
+console.log(subtract(10,5))

bbfe47adcaf0140e7b16abe8c72561c27c0c03f4
drakshayani04 <kdrakshayani04@gmail.com>
2025-09-06 15:00:50 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,46 @@
+let x=10,y=15;
+//arithmetic operators
+console.log(x+y)
+console.log(x-y)
+console.log(y/x)
+console.log(x%y)
+console.log(x*y)
+//assignment operators
+x+=1
+console.log(x)
+y-=2
+console.log(y)
+console.log(y*=1)
+console.log(x/=3)
+//comparision opertors
+a=10,b='10',c=7;
+console.log(a===b)
+console.log(a=c)
+console.log(a>=b)
+//logical operators
+let age=20
+console.log(age<=20&&age>=10)
+console.log(age<=20||age>=10)
+//string operators
+let firstName='drakshayani',lastName='kancharapu';
+console.log(firstName+" "+lastName)
+console.log(`welcome ${firstName}${lastName}`)
+//Ternary opertors
+let age1=19
+if(age>=18)
+    console.log("eligible to vote")
+else
+    console.log("not eligible to vote")
+let result=age1>=18?"eligible to vote":"you are not eligible"
+console.log(result)
+//increment operators
+count=6
+console.log(count++)
+console.log(count)
+console.log(++count)
+console.log(count)
+//type of operators
+console.log(typeof count)
+console.log(typeof "drakshayani")
+
+
